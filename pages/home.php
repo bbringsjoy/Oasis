ea9fc01633042abdd0378174556e564bde690359
Bia - integrado <biagsantana1000@gmail.com>
2025-08-04 20:42:17 -0300

    commit chale

--- pages/home.php
+++ pages/home.php
@@ -1,14 +1,13 @@
 <main>
     <h1> Loja </h1>
 
-<h3> Produtos em destaque </h3>
+<h3> Reservas em destaque </h3>
 
 <div class="card" style="width: 18rem;">
-  <img src="imagens/pokemon1.webp" class="card-img-top" alt="pokemon1">
+  <img src="imagens/chalé-urubici.webp" class="card-img-top" alt="pokemon1">
   <div class="card-body">
-    <h5 class="card-title">Piplup</h5>
-    <p class="card-text">A poor walker, it often falls down. However, its strong 
-        pride makes it puff up its chest without a care.</p>
+    <h5 class="card-title">Chalé - Urubici</h5>
+    <p class="card-text">Chalé localizado na cidade de Urubici - SC</p>
     <a href="#" class="btn btn-primary">Mais informações</a>
   </div>
 </div>
