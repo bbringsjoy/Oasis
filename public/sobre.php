cd6212e07fd26d0337e5313105584053bb7d066b
Francesco <francescofgris@gmail.com>
2025-09-22 01:38:42 -0300

    front end da sessão 'sobre'

---
+++ public/sobre.php
@@ -0,0 +1,67 @@
+<main>
+    
+<br><br>
+
+<div>
+<a class="reserve" onclick="history.back()">
+    voltar
+</a></div>
+<br><br><br><br>
+    <img src="imagens/Design sem nome.png" alt="" class="img-fluid rounded d-block col-6"
+            style="width: 100%;
+            height: auto;
+            padding: 0;
+            margin: 0;
+            border: 0;
+            ">
+        <img src="imagens/sobre2.png" alt="" style="width: 100%;">
+    <div class="sobre container>">
+       
+    <div>      
+            
+        </div>  
+        <div>
+            <p class="text-start col-6"
+            style="color: #3a8096;
+            font-weight: bold;
+            font-size: 30px;
+            text-align: center;"
+            >
+            Bem-vindo à nossa plataforma de reservas!<br>
+            Somos uma equipe dedicada a oferecer as melhores experiências de hospedagem. 
+            <br>Nossa missão é conectar viajantes a acomodações únicas e inesquecíveis.</p>
+            <p class="text-start col-6" 
+            style="color: #3a8096;
+            font-weight: bold;
+            font-size: 20px;
+            text-align: center;"
+            >
+            Nossa equipe é apaixonada por viagens e hospitalidade, e estamos aqui 
+            para garantir que sua estadia seja confortável e memorável. Se você tiver alguma dúvida ou precisar de assistência, não hesite em nos contatar.</p>
+        </div>
+        </div>
+        <br><br>
+        <div style="text-align: center;">
+            <a href="cadastro.php" class="reserve" >Reserve já</a></div>
+
+<style>
+.reserve {
+background-color: #5ba1b6ff;
+border-radius: 50px;
+color: #ffffffff;
+padding:10px 20px;
+text-align: center;
+transition: 1s;
+}
+.reserve:hover {
+background-color: #3a8096;
+transition: 1s;
+color: #ffffffff;
+}
+</style>
+
+
+
+
+<br><br><br><br><br><br><br><br><br><br><br>
+</main>
